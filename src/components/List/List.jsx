8064d04d31e0ec6167d5e624f473fb5051ee173e
Saahil Sood <saahils365@gmail.com>
2021-12-26 23:20:16 -0500

    Added basic skeleton for map

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select} from '@material-ui/core';
+import useStyles from './styles';
 
 const List = () => {
+    const classes = useStyles();
     return (
-        <h1>List</h1>
+        <div className={classes.container}>
+            <Typography variant="h4">Restaurants, Hotels, and Attractions</Typography>
+            <FormControl classNmae={classes.formControl}>
+                
+            </FormControl>
+        </div>
     )
 }
 
